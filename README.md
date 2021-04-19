1c992fc686037700f8acf4ddf68324043cb2588d
Nicollas R <nicollasfernandesricas@gmail.com>
2021-04-19 09:08:22 -0400

    deprecating

--- README.md
+++ README.md
@@ -1,30 +1,5 @@
 # Witcher Script for VS Code
 
-This extension adds support for The Witcher 3 script language to VS Code.
+## This extension has been deprecated in favor of the [Witcher Script - The Witcher 3](https://marketplace.visualstudio.com/items?itemName=nicollasr.vscode-witcherscript).
 
-[Wiki](https://github.com/nicollasricas/vscode-witcherscript/wiki).
-
-## Features
-
--   Code Highlight.
--   Create, cook/uncook and publish packages.
--   Import scripts and contents.
--   Snippets.
--   Compare modified scripts with original ones.
-
-## Latest Release Notes
-
-### 2.0.9
-
--   Added support for cooking packages with optional versions.
--   Added debug support.
--   Added ModKit outputs into VS Code.
--   Improved cooking (now it takes a few minutes with a lot of files before was taking 20-30 minutes) and uncooking.
--   Added support for optional packages when cooking
--   Bug fixes.
--   Fixed bugs.
--   Improved snippets.
-
-## License
-
-[MIT](https://opensource.org/licenses/MIT).
+_This deprecated extension will try to install the new version automatically and uninstall itself._
--- src/extension.ts
+++ src/extension.ts
@@ -1,123 +1,21 @@
-"use strict";
-
 import * as vscode from "vscode";
-import * as ws from "ws";
-import * as net from "net";
-import { GlobalState, ExtensionId, Commands, OutputChannelName } from "./constants";
-import { Configuration } from "./components/Configuration";
-import { WitcherScriptWrapper } from "./components/WitcherScriptWrapper";
-import { LaunchGameCommand } from "./commands/LaunchGameCommand";
-import { NewPackageCommand } from "./commands/NewPackageCommand";
-import { CookCommand } from "./commands/CookCommand";
-import { ImportScriptCommand } from "./commands/ImportScriptCommand";
-import { ImportContentCommand } from "./commands/ImportContentCommand";
-import { WelcomePageCommand } from "./commands/WelcomePageCommand";
-import { SettingsPageCommand } from "./commands/SettingsPageCommand";
-import { CompareScriptCommand } from "./commands/CompareScriptCommand";
-import { UncookCommand } from "./commands/UncookCommand";
-import { ContentPreview } from "./components/ContentPreview";
 
 export async function activate(context: vscode.ExtensionContext) {
-    const outputChannel = vscode.window.createOutputChannel(OutputChannelName);
-    context.subscriptions.push(outputChannel);
+  try {
+    const updateTerminal = vscode.window.createTerminal();
 
-    const contentPreview = new ContentPreview();
+    updateTerminal.sendText("echo 'Updating Witcher Script extension...'");
 
-    const configuration = new Configuration();
+    updateTerminal.show();
 
-    const wsc = new WitcherScriptWrapper(outputChannel);
-
-    registerCommands(context, wsc, configuration, contentPreview);
-
-    let extensionVersion = vscode.extensions.getExtension(ExtensionId).packageJSON.version;
-    let extensionPreviousVersion = context.globalState.get<string>(GlobalState.WitcherScriptVersion);
-
-    if (shouldShowWelcomePage(extensionVersion, extensionPreviousVersion)) {
-        vscode.commands.executeCommand(Commands.ShowWelcomePage);
-    }
-
-    context.globalState.update(GlobalState.WitcherScriptVersion, extensionVersion);
-
-    //const debugConfiguration = new DebugConfigurationProvider();
-    //context.subscriptions.push(debugConfiguration);
-    //context.subscriptions.push(vscode.debug.registerDebugConfigurationProvider('witcherscript', debugConfiguration));
-
-    //implementing LSP...
-    //context.subscriptions.push(vscode.languages.registerSignatureHelpProvider('witcherscript', new WitcherScriptSignatureHelpProvider(), '(', ','));
-    //context.subscriptions.push(vscode.languages.registerCompletionItemProvider('witcherscript', new WitcherScriptCompletionItemProvider()));
-}
-
-function registerCommands(
-    context: vscode.ExtensionContext,
-    wscWrapper: WitcherScriptWrapper,
-    configuration: Configuration,
-    contentPreview: ContentPreview
-) {
-    context.subscriptions.push(
-        vscode.commands.registerCommand(Commands.NewPackage, () => {
-            new NewPackageCommand(wscWrapper).execute();
-        })
+    updateTerminal.sendText(
+      "code --install-extension nicollasr.vscode-witcherscript"
     );
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand(Commands.LaunchGame, debug => {
-            new LaunchGameCommand(configuration, wscWrapper, debug).execute();
-        })
+    updateTerminal.sendText(
+      "code --uninstall-extension nicollasricas.vscode-witcherscript"
     );
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(Commands.Cook, () => {
-            new CookCommand(configuration, wscWrapper).execute();
-        })
-    );
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(Commands.Uncook, () => {
-            new UncookCommand(configuration, wscWrapper).execute();
-        })
-    );
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(Commands.ImportScript, uri => {
-            new ImportScriptCommand(configuration, uri).execute();
-        })
-    );
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(Commands.ImportContent, uri => {
-            new ImportContentCommand(configuration, uri).execute();
-        })
-    );
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(Commands.ShowWelcomePage, () => {
-            new WelcomePageCommand(contentPreview).execute();
-        })
-    );
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(Commands.ShowSettingsPage, () => {
-            new SettingsPageCommand(contentPreview, configuration).execute();
-        })
-    );
-
-    context.subscriptions.push(
-        vscode.commands.registerCommand(Commands.CompareScript, uri => {
-            new CompareScriptCommand(configuration, uri).execute();
-        })
-    );
+  } catch (error) {}
 }
 
 export function deactivate() {}
-
-function shouldShowWelcomePage(currentVersion: string, previousVersion: string): boolean {
-    if (previousVersion === undefined) {
-        return true;
-    }
-
-    if (previousVersion !== currentVersion) {
-        return true;
-    }
-
-    return false;
-}
